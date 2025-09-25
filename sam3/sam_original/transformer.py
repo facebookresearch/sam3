e7212d7cc4e9c048012b920428437665432d902b
kalyanvasudev <kalyan051993@gmail.com>
2025-09-25 17:19:27 -0400

    cleanup sam_original transformer.py

--- sam3/sam_original/transformer.py
+++ sam3/sam_original/transformer.py
@@ -252,7 +252,7 @@ class Attention(nn.Module):
         # ):
         # Let's trust the dispatcher....
         if self.use_fa3:
-            from onevision.perflib.fa3 import flash_attn_func
+            from sam3.perflib.fa3 import flash_attn_func
 
             assert dropout_p == 0.0
             out = flash_attn_func(
@@ -347,7 +347,7 @@ class RoPEAttention(Attention):
         # ):
         # Let's trust the dispatcher....
         if self.use_fa3:
-            from onevision.perflib.fa3 import flash_attn_func
+            from sam3.perflib.fa3 import flash_attn_func
 
             assert dropout_p == 0.0
             out = flash_attn_func(
