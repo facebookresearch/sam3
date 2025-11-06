978a80204c45ff7b2272d9a1aedd7f05e553a568
Shoubhik Debnath <shoubhikdn@fb.com>
2025-11-06 11:43:42 +0000

    Update COCO evaluator to reproduce arxiv paper numbers

--- sam3/train/eval/demo_eval.py
+++ sam3/train/eval/demo_eval.py
@@ -14,11 +14,11 @@ import pycocotools.mask as maskUtils
 
 from pycocotools.cocoeval import COCOeval
 
+from scipy.optimize import linear_sum_assignment
+
 from sam3.train.eval.coco_eval import CocoEvaluator
 from sam3.train.masks_ops import compute_F_measure
 from sam3.train.utils.distributed import is_main_process
-
-from scipy.optimize import linear_sum_assignment
 
 
 class DemoEval(COCOeval):
@@ -494,6 +494,7 @@ class DemoEvaluator(CocoEvaluator):
         average_by_rarity=False,
         gather_pred_via_filesys=False,
         exhaustive_only=False,
+        all_exhaustive_only=True,
         compute_JnF=False,
         metrics_dump_dir: Optional[str] = None,
     ):
@@ -508,6 +509,7 @@ class DemoEvaluator(CocoEvaluator):
             # average_by_rarity=average_by_rarity,
             gather_pred_via_filesys=gather_pred_via_filesys,
             exhaustive_only=exhaustive_only,
+            all_exhaustive_only=all_exhaustive_only,
             metrics_dump_dir=metrics_dump_dir,
         )
 
